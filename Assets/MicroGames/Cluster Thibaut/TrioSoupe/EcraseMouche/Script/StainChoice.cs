7b91ced5e41ecce2d01740adfeac3a0e7a508956
Arthur <arthurgpro@outlook.fr>
2020-12-11 11:58:20 +0100

    Ajout d'un namespace et d'un sumary
    Ne change rien au rendu précédent, c'est juste pour la nomenclature demandé par les Progs

--- Assets/MicroGames/Cluster Thibaut/TrioSoupe/EcraseMouche/Script/StainChoice.cs
+++ Assets/MicroGames/Cluster Thibaut/TrioSoupe/EcraseMouche/Script/StainChoice.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StainChoice : MonoBehaviour
+namespace Soupe
 {
-
-    [SerializeField]
-    private List<Sprite> spritesOfJam;
-    private int numberMax;
-    private SpriteRenderer rend;
-
-    // Start is called before the first frame update
-    void Start()
+    namespace EcraseMouche
     {
-        rend = GetComponent<SpriteRenderer>();
-        numberMax = Random.Range(0, spritesOfJam.Count);
-        rend.sprite = spritesOfJam[numberMax];
-    }
+        /// <summary>
+        /// Arthur Galland
+        /// </summary>
+        public class StainChoice : MonoBehaviour
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            [SerializeField]
+            private List<Sprite> spritesOfJam;
+            private int numberMax;
+            private SpriteRenderer rend;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                rend = GetComponent<SpriteRenderer>();
+                numberMax = Random.Range(0, spritesOfJam.Count);
+                rend.sprite = spritesOfJam[numberMax];
+            }
+
+        }
     }
 }
+
+
